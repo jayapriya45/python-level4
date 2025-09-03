0b1e1c256e9582beeab320843f5e1d4c0cc8993d
jayapriya45 <j64323768@gmail.com>
2025-09-03 16:06:25 +0530

    py17a

---
+++ py17a.py
@@ -0,0 +1,10 @@
+print("SUM OF N NUMBERS")
+print("----------------")
+n=int(input("Enter the number:"))
+print("Result")
+sum=0
+for i in range(1,n+1):
+    print("+",+i)
+    sum=sum+i
+    print("Sum")
+    print(sum)
