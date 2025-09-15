6d1a34043ac2f6e986fc1a08a8b6ed9c5db93d37
jayapriya45 <j64323768@gmail.com>
2025-09-15 10:07:31 +0530

    py19

---
+++ py19.py
@@ -0,0 +1,11 @@
+print("Reverse number")
+print("---------------")
+n=int(input("Enter the number:"))
+print("Result")
+print("Reverse number:")
+while(n>0):
+  r=n%10
+  n=n/10
+  print(r)
+  print(n)
+  n=int(n)
